a557be28ba697305a46bc70ed0159fe68f63adf2
dindiongco <dindiongco@qa.com>
2023-06-27 11:51:03 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
-
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="primary-footer | padding-block-700">
+      <div className="container">
+        <div className="primary-footer-wrapper">
+          <div className="primary-footer-logo-social">
+            <a href="#">
+            <svg className="logo" width={146} height={24}>
+              <use xlinkHref="../src/assets/logo.svg"></use>
+            </svg>
+            </a>
+            <ul className='social-list' role="list" aria-label="social links">
+              <li><a aria-label="facebook" href="#"><svg className="social-icon">
+              <use xlinkHref="../src/assets/social-icons.svg#icon-facebook"></use>
+            </svg></a></li>
+              <li><a aria-label="twitter" href="#"><svg className="social-icon">
+              <use xlinkHref="../src/assets/social-icons.svg#icon-twitter"></use>
+            </svg></a></li>
+              <li><a aria-label="pinterest" href="#"><svg className="social-icon">
+              <use xlinkHref="../src/assets/social-icons.svg#icon-pinterest"></use>
+            </svg></a></li>
+              <li><a aria-label="instagram" href="#"><svg className="social-icon">
+              <use xlinkHref="../src/assets/social-icons.svg#icon-instagram"></use>
+            </svg></a></li>
+            </ul>
+            </div>
+            </div>
+            </div>
+    </footer>
   )
 }
